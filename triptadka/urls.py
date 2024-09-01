627cb9c037ed5a0927c22d5214e55366888f9a77
Saumya Ranjan Mohapatra <saumyaranjanmohapatra978@gmail.com>
2024-09-01 20:12:35 +0530

    update

--- triptadka/urls.py
+++ triptadka/urls.py
@@ -21,6 +21,9 @@ from django.urls import path, include
 from django.conf import settings
 from  django.conf.urls.static import static
 
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('photos.urls')),
@@ -28,3 +31,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
